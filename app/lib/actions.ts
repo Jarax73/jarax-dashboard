e1dc6b57409b325d35605c5f6cbd5b2b79f6d7d9
Jarax73 <jaraxkapila73@gmail.com>
2025-03-17 21:20:17 +0100

    Add metadata to the dashboard application

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,7 +59,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
           VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
   } catch (error) {
-    // We'll log the error to the console for now
+    console.error(error);
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
